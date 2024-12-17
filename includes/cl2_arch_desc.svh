7a145201021a590a27762b04fd4a8948f1cebc06
maksyuki <maksyuki@126.com>
2024-12-17 22:01:32 +0800

    feat: modify arch desc header

--- includes/cl2_arch_desc.svh
+++ includes/cl2_arch_desc.svh
@@ -1,6 +1,8 @@
 `ifndef CL2_ARCH_DESC_SVH
 `define CL2_ARCH_DESC_SVH
 
+`define CL2_INST_SIZE     32
+`define CL2_PC_SIZE       32
 `define CL2_XLEN          32
 `define CL2_REGFILE_NUM   32 // NOTE: just for I extension
 `define CL2_REGFILE_WIDTH $clog2(`CL2_REGFILE_NUM)
